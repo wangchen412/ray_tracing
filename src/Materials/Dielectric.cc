7d5e55e0c006e5c4504728fa4e3d93f64cce0b81
Chen Wang <wangchen412@gmail.com>
2023-09-28 22:12:15 -0600

    Total internal reflection.

--- src/Materials/Dielectric.cc
+++ src/Materials/Dielectric.cc
@@ -8,8 +8,13 @@ bool Dielectric::scatter(const Ray& incident, const HitRecord& rec,
                          Color& atten, Ray& scattered) const {
   atten = Color(1, 1, 1);
   double refraction_ratio = rec.front_face ? (1 / ir_) : ir_;
-  auto refracted =
-      refract(incident.unit_direction(), rec.normal, refraction_ratio);
-  scattered = Ray(rec.position, refracted);
+
+  auto unit_direction = incident.unit_direction();
+  auto c = min(-unit_direction.dot(rec.normal), 1.0);
+  auto s = sqrt(1 - c * c);
+  auto direction = refraction_ratio * s > 1.0
+                       ? reflect(unit_direction, rec.normal)
+                       : refract(unit_direction, rec.normal, refraction_ratio);
+  scattered = Ray(rec.position, direction);
   return true;
 }
